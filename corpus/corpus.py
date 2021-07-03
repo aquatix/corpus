242ba2c6554ffeb8b27314834795ca7ff908bd18
Michiel Scholten <michiel@diginaut.net>
2021-07-03 13:12:55 +0200

    Work on removing existing Backlinks section from file

--- corpus/corpus.py
+++ corpus/corpus.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from . import links
+# from . import links
+import links
 
 
 def parse_files(all_files):
@@ -10,10 +11,12 @@ def parse_files(all_files):
     for textfile in all_files:
         with open(textfile, 'r') as f:
             text = f.read()
+            print(links.split_backlink_section_from_markdown(text))
             links_in_file = links.extract_links(text)
             print(links_in_file)
 
 
-all_files = list(Path.home().joinpath("phren").rglob("*.[mM][dD]"))
-print(all_files)
-parse_files(all_files)
+# all_files = list(Path.home().joinpath("phren").rglob("*.[mM][dD]"))
+# print(all_files)
+# parse_files(all_files)
+parse_files(['/home/mbscholt/phren/homeautomation/lights.md'])
--- corpus/links.py
+++ corpus/links.py
@@ -10,3 +10,18 @@ def extract_links(string):
     links = list(set(re.findall(r'href=[\'"]?([^\'" >]+)', html)))
     links = list(filter(lambda l: l[0] != "{", links))
     return links
+
+
+def split_backlink_section_from_markdown(string):
+    """Split the backlinks section from the markdown file, which should be the last, but doesn't have to be :)
+    """
+    # Find the section with Backlinks; if there are multiple for some reason, pick the last one
+    markdown_parts = string.rsplit("# Backlinks")
+    print(markdown_parts)
+    backlinks_removed = markdown_parts[1].split('#')
+    print(backlinks_removed)
+    return markdown_parts
+
+
+def find_backlinks_in_markdown(string):
+    return string
